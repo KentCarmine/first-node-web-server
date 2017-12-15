896b8ef48907b78d3a2c5a5c30abf8c5965056ff
Kent Carmine <kentcarmine@gmail.com>
2017-12-15 15:07:30 -0600

    Set up basic web server

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 'use strict';
 
 const express = require('express');
+
+let app = express();
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello, Express!</h1>');
+    res.send({
+      name: "Kent",
+      likes: ['RPGs', 'critical thinking', 'coding']
+    });
+});
+
+app.get('/about', (req, res) => {
+  res.send("About page");
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: "Error handling request!"
+  });
+});
+
+app.listen(3000);
