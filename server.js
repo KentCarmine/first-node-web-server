f7092b0f4550040265f8e374e4238ae68c1e5e2f
Kent Carmine <kentcarmine@gmail.com>
2017-12-15 16:17:39 -0600

    Added hbs templating

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 'use strict';
 
 const express = require('express');
+const hbs = require('hbs');
 
 let app = express();
-
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello, Express!</h1>');
-    res.send({
-      name: "Kent",
-      likes: ['RPGs', 'critical thinking', 'coding']
+    res.render('home.hbs', {
+      pageTitle: "Home Page",
+      welcomeMessage: "Welcome to the site"
     });
 });
 
 app.get('/about', (req, res) => {
-  res.send("About page");
+  res.render('about.hbs', {
+    pageTitle: "About Page"
+  });
 });
 
 app.get('/bad', (req, res) => {
